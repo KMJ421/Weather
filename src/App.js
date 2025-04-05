db0396199406ce086e211d0e5308da83524b3f84
minji <minji03421@naver.com>
2025-04-06 02:02:15 +0900

    배포오류수정

--- src/App.js
+++ src/App.js
@@ -28,23 +28,24 @@ function App() {
     setLoading(false)
   };
 
-  const getWeatherByCity = async() => {
+  const getWeatherByCity = useCallback(async () => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bbe61974a6329d0b9a5524334656229&units=metric`;
-    setLoading(true)
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
     setLoading(false);
-  }
+  }, [city]);
+  
 
   useEffect(() => {
-    if(city==""){
+    if(city===""){
       getCurrentLocation()
     }
     else{
       getWeatherByCity();
     }
-  }, [city])
+  }, [city, getCurrentLocation, getWeatherByCity])
 
 
   return (
